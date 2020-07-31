d52d4f53b14e8088f49c8a4d5d544b07fdede3dd
iQaiserAbbas <qaiserabbas889@yahoo.com>
2020-07-31 21:06:28 +0500

    the print function and object string

---
+++ 02-Welcome-to-Python/comments.py
@@ -0,0 +1,7 @@
+print("Good morning")
+#print("Good afternoon")
+print("Good night")
+
+# This will perform a mathematical calculation
+print(6 + 6) # Adds together 6+6
+
---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,19 @@
+"chicken"
+'chicken'
+"warriorsfan6!"
+"$2.00"
+
+"You're right"
+'Shakespeare once wrote "To be or not to be" in one of his plays'
+
+" "
+""
+''
+
+"5"
+5
+
+"""Hello
+my name is
+Qaiser
+"""
