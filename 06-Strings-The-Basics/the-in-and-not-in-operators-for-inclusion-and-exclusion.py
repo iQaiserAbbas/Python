9db0e43e890b0438471b32a09a2f89d25ab4fe13
iQaiserAbbas <qaiserabbas889@yahoo.com>
2020-08-04 13:50:31 +0500

    in, not in operator with string slicing

---
+++ 06-Strings-The-Basics/the-in-and-not-in-operators-for-inclusion-and-exclusion.py
@@ -0,0 +1,16 @@
+announcement = "The winners of the prize are Qaiser, Andy, and Adam"
+
+print("Qaiser" in announcement)
+print("Steven" in announcement)
+print("qaiser" in announcement)
+print(" " in announcement)
+print("," in announcement)
+
+print()
+
+print("Qaiser" not in announcement)
+print("Steven" not in announcement)
+print("qaiser" not in announcement)
+print(" " not in announcement)
+print("," not in announcement)
+
