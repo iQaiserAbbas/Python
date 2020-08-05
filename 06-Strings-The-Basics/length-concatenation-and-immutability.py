0e392c83cba0cdff5958388a8498fdf744efd825
iQaiserAbbas <qaiserabbas889@yahoo.com>
2020-08-05 13:25:02 +0500

    escape characters and concatenation files

---
+++ 06-Strings-The-Basics/length-concatenation-and-immutability.py
@@ -0,0 +1,17 @@
+print(len("Python"))
+print(len("programming"))
+print(len("!@#$"))
+print(len(" "))
+print(len(""))
+
+# print(len(4))
+# print(len(3.14))
+
+print("Qaiser" + "Abbas")
+print("Qaiser " + "Abbas")
+print("Qaiser" + " Abbas")
+print("Qaiser" + " " + "Abbas")
+
+print("a" "b" "c")
+
+print("---" * 30)
