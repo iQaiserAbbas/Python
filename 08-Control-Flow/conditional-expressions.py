857cf503509a77e7643a15cdf0ca8320a6476c88
iQaiserAbbas <qaiserabbas889@yahoo.com>
2020-08-11 10:41:43 +0500

    Conditional Expressions concept

---
+++ 08-Control-Flow/conditional-expressions.py
@@ -0,0 +1,10 @@
+zip_code = "75260"
+
+# check
+# if len(zip_code) == 5:
+#     check = "Valid"
+# else:
+#     check = "Invalid"
+
+check = "Valid" if len(zip_code) == 5 else "Invalid"
+print(check)
