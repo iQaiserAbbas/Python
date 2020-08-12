b3a8224166757ef4b5507261ec8ea9ed6e0efecc
iQaiserAbbas <qaiserabbas889@yahoo.com>
2020-08-12 14:43:39 +0500

    string_theory and divisible_by_three_and_four exercise

---
+++ 08-Control-Flow/Exercise 02.py
@@ -0,0 +1,28 @@
+# Define a divisible_by_three_and_four function that accepts a number as its argument. 
+# It should return True if the number is evenly divisible by both 3 and 4 . 
+# It should return False otherwise.
+#
+# divisible_by_three_and_four(3)   => False
+# divisible_by_three_and_four(4)   => False
+# divisible_by_three_and_four(12)  => True
+# divisible_by_three_and_four(18)  => False
+# divisible_by_three_and_four(24)  => True
+def divisible_by_three_and_four(value):
+    if value % 3 == 0 and value % 4 == 0:
+        return True
+    else:
+        return False
+
+ # Declare a string_theory function that accepts a string as an argument. 
+# It should return True if the string has more than 3 characters 
+# and starts with a capital “S”. It should return False otherwise.
+#
+# string_theory("Sansa") => True
+# string_theory("Story") => True
+# string_theory("See") => False
+# string_theory("Fable") => False
+def string_theory(word):
+    if len(word) >= 4 and word[0] == "S":
+        return True
+    else:
+        return False
