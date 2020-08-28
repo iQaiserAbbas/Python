7100417191f59896bfa8daa27a815b88f9b22038
iQaiserAbbas <QaiserAbbas889@yahoo.com>
2020-08-28 07:39:22 +0500

    More list iteration: Smallest_num, concatenate, super_Sum

---
+++ 10-Lists-Iteration/exercise-02.py
@@ -0,0 +1,58 @@
+# Define a smallest_number function  that accepts a list of numbers.
+# It should return the smallest value in the list.
+#
+# smallest_number([1, 2, 3])     => 1
+# smallest_number([3, 2, 1])     => 1
+# smallest_number([4, 5, 4])     => 4
+# smallest_number([-3, -2, -1])  => -3
+
+def smallest_number(numbers):
+    total = numbers[0]
+    for number in numbers:
+        if number < total:
+            total = number
+    return total
+
+
+
+# Define a concatenate function that accepts a list of strings. 
+#
+# The function should return a concatenated string which consists of
+# all list elements whose length is greater than 2 characters.
+#
+# concatenate(["abc", "def", "ghi"])      => "abcdefghi"
+# concatenate(["abc", "de", "fgh", "ic"]) => "abcfgh"
+# concatenate(["ab", "cd", "ef", "gh"])   => ""
+def concatenate(strs):
+    answer = ""
+    for str in strs:
+        if len(str) > 2:
+            answer = answer + str
+    return answer
+print(concatenate(["abc", "def", "ghi"]))
+print(concatenate(["abc", "de", "fgh", "ic"]))
+print(concatenate(["ab", "cd", "ef", "gh"]))
+
+
+
+# Define a super_sum function that accepts a list of strings. 
+# The function should sum the index positions of the first occurence of the letter “s” in each word. 
+#
+# Not every word is guaranteed to have an “s”.
+# Don’t use "sum" as a variable name as it’s a built-in keyword.
+#
+# super_sum([])                                 => 0
+# super_sum(["mustache"])                       => 2
+# super_sum(["mustache", "greatest"])           => 8
+# super_sum(["mustache", "pessimist"])          => 4
+# super_sum(["mustache", "greatest", "almost"]) => 12
+def super_sum(strs):
+    total = 0
+    for str in strs:
+        if "s" in str:
+            index = str.index("s")
+            total = total + index
+    return total
+print(super_sum(["mustache"]))
+print(super_sum(["mustache", "pessimist"]))
+print(super_sum(["mustache", "greatest", "almost"]))
