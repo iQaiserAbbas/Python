cade79ef32e1141ae11545fae040604ff68adeeb
iQaiserAbbas <QaiserAbbas889@yahoo.com>
2020-08-31 16:00:58 +0500

    The break keyword

---
+++ 10-Lists-Iteration/the-break-keyword.py
@@ -0,0 +1,14 @@
+print(3 in [1, 2, 3, 4, 5, 3])
+
+def contains(values, target):
+    found = False
+
+    for value in values:
+        print(value)
+        if value == target:
+            found = True
+            break
+
+    return found
+
+print(contains([1, 2, 3, 4, 5], 3))
