242c98bce232fbda33645fce0a9ae2877b9dea2a
iQaiserAbbas <QaiserAbbas889@yahoo.com>
2020-09-01 22:18:04 +0500

    range function

---
+++ 10-Lists-Iteration/the-range-function.py
@@ -0,0 +1,11 @@
+# for number in range(11):
+#     print(number)
+
+# for number in range(3, 9):
+#     print(number)
+
+# for number in range(10, 101, 8):
+#     print(number)
+
+for number in range(99, -1, -11):
+    print(number)
