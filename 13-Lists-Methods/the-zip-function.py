d4bbdc07306916c7b6d1a722344bbecc339102fc
iQaiserAbbas <QaiserAbbas889@yahoo.com>
2020-09-17 00:02:37 +0500

    The zip function in python

---
+++ 13-Lists-Methods/the-zip-function.py
@@ -0,0 +1,10 @@
+breakfasts = ["Eggs", "Cereal", "Banana"]
+lunches = ["Sushi", "Chicken Teriyaki", "Soup"]
+dinners = ["Steak", "Meatballs", "Pasta"]
+
+# print(zip(breakfasts, lunches, dinners))
+# print(type(zip(breakfasts, lunches, dinners)))
+# print(list(zip(breakfasts, lunches, dinners)))
+
+for breakfast, lunch, dinner in zip(breakfasts, lunches, dinners):
+    print(f"My meal for today was {breakfast} and {lunch} and {dinner}.")
