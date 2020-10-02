2121d801d1d516df13b5f75ec49dc7d95ff94733
AdnanSE <72250612+AdnanSE@users.noreply.github.com>
2020-10-02 13:42:37 +0500

    join.py added

---
+++ join.py
@@ -0,0 +1,9 @@
+address = ["500 Fifth Avenue", "New York", "NY", "10036"]
+
+print(",".join(address))
+print(", ".join(address))
+print("".join(address))
+
+print("-".join(["555", "123", "4567"]))
+print("|".join(["555", "123", "4567"]))
+print("***".join(["555", "123", "4567"]))
